dccba2764764bd760d7a43474eadcdb40d51731d
Amrik-Bhadra <amrik.bhadra@gmail.com>
2025-06-25 09:31:45 +0530

    Save local changes before pulling

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://sequelizer-api.devmadeeasy.com/api',
+    // baseURL: 'https://sequelizer-api.devmadeeasy.com/api',
+    baseURL: 'http://localhost:3000/api',
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
